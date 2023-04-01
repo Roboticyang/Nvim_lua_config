257b07e49a722bb46eb48c59c2558da2ba2cca9c
Lao Yang <robotic.yang.wang@gmail.com>
2023-04-01 15:04:39 -0400

    refactor: update the deprecated barbar codes

--- lua/barbar-config/init.lua
+++ lua/barbar-config/init.lua
@@ -1,11 +1,24 @@
-vim.g.bufferline = {
-  -- Enable/disable animations
-  animation = true,
+-- following code deprecated
+-- vim.g.bufferline = {
+--   -- Enable/disable animations
+--   animation = true,
+--
+--   -- Enable/disable close button
+--   closable = true,
+--
+--   -- Configure icons on the bufferline.
+--   icon_separator_active = '',
+--   icon_separator_inactive = '',
+-- }
 
-  -- Enable/disable close button
-  closable = true,
-
-  -- Configure icons on the bufferline.
-  icon_separator_active = '',
-  icon_separator_inactive = '',
+-- use following instead
+require'barbar'.setup {
+	animation = true,
+	clickable = true, -- key words updated
+	icons = {
+		separator = {left = ''}, -- this is the active
+		inactive = {
+			separator = {left = ''},
+		},
+	},
 }
